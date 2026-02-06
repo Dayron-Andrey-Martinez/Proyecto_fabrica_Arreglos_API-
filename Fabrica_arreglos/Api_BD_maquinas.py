e32de3d666f3a6d10201afeac843df61d0133071
Dayron-Andrey-Martinez <dayronandreymartinez@gmail.com>
2026-02-06 08:50:22 -0500

    Enhance Api_BD_maquinas with new methods

--- Fabrica_arreglos/Api_BD_maquinas.py
+++ Fabrica_arreglos/Api_BD_maquinas.py
@@ -2,9 +2,9 @@ class Api_BD_maquinas:
     def __init__(self):
         self.Api_maquina = [
             [ "codigo","nombre maquina","Modelo Maquina","Estado maquina"],
-            ["cod 1834", "brazo mecanico","xrt2333","apagada"],
-            ["cod 2952","cinta transportadora","M600","requiere mantenimiento"],
-            ["cod 5948", "Monobrazo","AL100","Encendida"]
+            ["cod 1234", "brazo mecanico","x200","apagada"],
+            ["cod 2352","cinta transportadora","zx400","requiere mantenimiento"],
+            ["cod 5648", "Monobrazo","jk100","Encendida"]
         ]
         
     def imprimir_info(self):
@@ -13,3 +13,19 @@ class Api_BD_maquinas:
     
     def buscar_info(self):
         return self.Api_maquina[2][2]        
+    
+    def agregar_maquina(self,nueva_maquina):
+        self.Api_maquina.append(nueva_maquina)
+
+   
+    def guardar_varias_maquinas(self,nueva_lista):
+        self.Api_maquina.extend(nueva_lista)
+
+
+    def eliminar_ele_maquina(self,data_i):
+        self.Api_maquina.remove(data_i)
+
+    def cambiar_ele_maquina(sefl):
+        sefl.Api_maquina.pop()
+
+ 
